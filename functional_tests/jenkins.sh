3bd0bca615d8e1b2059e6d7ea875588806827254
Daniel Yazbek <daniel@intersect.org.au>
2014-07-03 10:54:00 +1000

    Create script to execute tests in Jenkins

--- functional_tests/jenkins.sh
+++ functional_tests/jenkins.sh
@@ -7,6 +7,7 @@
 #   BCCVL_TEST_URL - the base URL to the bccvl site to test
 #   BCCVL_TEST_USERNAME - the username to use when logging into the bccvl (must have admin rights)
 #   BCCVL_TEST_PASSWORD - the password to use when logging into the bccvl
+#   BCCVL_TEST_VIRTUAL_DISPLAY - true to use a virtual display while executing tests, false otherwise.
 #   BCCVL_TEST_IMPLICIT_WAIT (optional) - The number of seconds to wait while selenium is looking for web elements
 ###
 
