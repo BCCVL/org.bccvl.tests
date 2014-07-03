2f843f604074e080730d8c1dcca378cfa37fe715
Daniel Yazbek <daniel@intersect.org.au>
2014-07-03 12:36:57 +1000

    More cleanup of test cases

--- functional_tests/tests/bccvl_testcase.py
+++ functional_tests/tests/bccvl_testcase.py
@@ -1,10 +1,7 @@
 import unittest
 import os
-import time
 from pyvirtualdisplay import Display
 from selenium import webdriver
-from selenium.webdriver.support.select import Select
-from selenium.common.exceptions import NoSuchElementException
 
 
 # Base Test Cases used to test BCCVL
@@ -36,46 +33,8 @@ class BCCVLTestCase(unittest.TestCase):
         self.driver.maximize_window()
         # Go to the bccvl homepage
         self.driver.get(self.url)
-        self.experiments = []
 
     def tearDown(self):
-        # if experiments were created during this test then delete them
-        for experiment in self.experiments:
-            self.delete_experiment(experiment)
         if self.display:
             self.display.stop()
         self.driver.quit()
-
-    def delete_dataset(self, title):
-        self.driver.get(self.url+'/datasets/manage_main')
-        # TODO: write this method when doing datasets page tests
-
-    def delete_experiment(self, title):
-        self.driver.get(self.url+'/experiments/manage_main')
-        end_time = time.time() + 60
-        while time.time() <= end_time:
-            if self._check_exists_by_xpath("//option[@value='" + title + "']"):
-                break
-            self.driver.refresh()
-        select = Select(self.driver.find_element_by_xpath("//select[@name='ids:list']"))
-        select.select_by_value(title)
-        self.driver.find_element_by_name("manage_delObjects:method").click()
-
-    # Delete all experiments in the BCCVL
-    def delete_all_experiments(self):
-        self.driver.get(self.url+'/experiments/manage_main')
-        self.driver.find_element_by_name('selectButton').click()
-        self.driver.find_element_by_name('manage_delObjects:method').click()
-
-    def generate_timestamp(self):
-        return str(int(time.time()))
-
-    # Note: This method will block if the element was not immediately visible.
-    # The amount of time that this will block depends on the call to self.driver.implicitly_wait in this class'
-    # setUp(self)
-    def _check_exists_by_xpath(self, xpath):
-        try:
-            self.driver.find_element_by_xpath(xpath)
-        except NoSuchElementException:
-            return False
-        return True
