ff9cd3bda591a103f8017f2a8712080aa3476129
Stanley Hon <stanleyhon348@gmail.com>
2014-07-17 12:22:02 +1000

    Added two new complex SDM experiment tests. Added a helper script in tools/ that helps convert experiment results page into file existance assertions.

--- functional_tests/tests/test_sdm_experiment.py
+++ functional_tests/tests/test_sdm_experiment.py
@@ -1353,6 +1353,204 @@ class TestSDMExperiment(ExperimentTestCase):
         # Cleanup
         self.delete_experiment(experiment_name)
 
-    # Test ANN, BRT, FDA and GLM together.
-    def test_ann_brt_fda_glm_1km_quad(self):
-        pass
+    # Test 4 algorithms together.
+    # Triple Climate Layers
+    # 1KM
+    def test_brt_ct_fda_gbm_1km_quad(self):
+        homepage = Homepage(self.driver)
+        login_page = homepage.click_login()
+        homepage = login_page.valid_login(self.username, self.password)
+        experiment_page = homepage.click_experiments()
+        new_sdm_page = experiment_page.click_new_sdm_experiment()
+
+        experiment_name = "test_brt_ct_fda_gbm_1km_quad_" + generate_timestamp()
+
+        new_sdm_page.enter_experiment_name(experiment_name)
+        new_sdm_page.enter_experiment_description('test_brt_ct_fda_gbm_1km_quad')
+        new_sdm_page.select_configuration()
+        new_sdm_page.select_sdm_algorithm('Classification Tree')
+        new_sdm_page.select_sdm_algorithm('Boosted Regression Tree')
+        new_sdm_page.select_sdm_algorithm('Flexible Discriminant Analysis')
+        new_sdm_page.select_sdm_algorithm('Generalized Boosting Model')
+        new_sdm_page.select_occurrences()
+        new_sdm_page.select_occurrences_dataset('Koala - Mini occurrence dataset for Redland City')
+        new_sdm_page.select_absences()
+        new_sdm_page.select_absences_dataset('Koala - Mini absence dataset for Redland City')
+        new_sdm_page.select_environment()
+
+        new_sdm_page.select_current_climate_layers('Current climate layers for Redland City, 30" (~1km)')
+        new_sdm_page.select_environmental_datasets('Current climate layers for Redland City, 30" (~1km)',
+                                                   'B01 - Annual Mean Temperature')
+        new_sdm_page.select_environmental_datasets('Current climate layers for Redland City, 30" (~1km)',
+                                                   'B02 - Mean Diurnal Range (Mean of monthly (max temp - min temp))')
+        new_sdm_page.select_environmental_datasets('Current climate layers for Redland City, 30" (~1km)',
+                                                   'B14 - Precipitation of Driest Month')
+        new_sdm_page.select_run()
+        experiment_view = new_sdm_page.select_review_start_experiment()
+
+        # Wait until completion
+        experiment_view.wait_for_experiment_to_complete()
+        self.assertTrue(experiment_view.has_completed_successfully())
+
+        self.assertTrue(experiment_view.has_results_header(experiment_name))
+
+        self.assertEqual(41, experiment_view.get_num_output_files())
+
+        self.assertTrue(experiment_view.has_result_file('dismo.eval.object.RData', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('Phascolarctus.cinereus.model.object.RData', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_01_response.png', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_02_response.png', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_14_response.png', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('results.html', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('AUC.png', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('brt.Rout', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('combined.modelEvaluation.csv', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('maxent_like_VariableImportance.csv', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('biomod2_like_VariableImportance.csv', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('results.html.zip', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_Phascolarctus.cinereus.tif', 'brt'))
+        self.assertTrue(experiment_view.has_result_file('pstats.json', 'brt'))
+
+        self.assertTrue(experiment_view.has_result_file('mean_response_curves.png', 'cta'))
+        self.assertTrue(experiment_view.has_result_file('pROC.png', 'cta'))
+        self.assertTrue(experiment_view.has_result_file('cta.Rout', 'cta'))
+        self.assertTrue(experiment_view.has_result_file('biomod2.modelEvaluation.csv', 'cta'))
+        self.assertTrue(experiment_view.has_result_file('combined.modelEvaluation.csv', 'cta'))
+        self.assertTrue(experiment_view.has_result_file('model.object.RData.zip', 'cta'))
+        self.assertTrue(experiment_view.has_result_file('pstats.json', 'cta'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_Phascolarctus.cinereus.tif', 'cta'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_ClampingMask.tif', 'cta'))
+
+        self.assertTrue(experiment_view.has_result_file('mean_response_curves.png', 'fda'))
+        self.assertTrue(experiment_view.has_result_file('pROC.png', 'fda'))
+        self.assertTrue(experiment_view.has_result_file('fda.Rout', 'fda'))
+        self.assertTrue(experiment_view.has_result_file('biomod2.modelEvaluation.csv', 'fda'))
+        self.assertTrue(experiment_view.has_result_file('combined.modelEvaluation.csv', 'fda'))
+        self.assertTrue(experiment_view.has_result_file('model.object.RData.zip', 'fda'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_Phascolarctus.cinereus.tif', 'fda'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_ClampingMask.tif', 'fda'))
+        self.assertTrue(experiment_view.has_result_file('pstats.json', 'fda'))
+
+        self.assertTrue(experiment_view.has_result_file('mean_response_curves.png', 'gbm'))
+        self.assertTrue(experiment_view.has_result_file('pROC.png', 'gbm'))
+        self.assertTrue(experiment_view.has_result_file('gbm.Rout', 'gbm'))
+        self.assertTrue(experiment_view.has_result_file('biomod2.modelEvaluation.csv', 'gbm'))
+        self.assertTrue(experiment_view.has_result_file('combined.modelEvaluation.csv', 'gbm'))
+        self.assertTrue(experiment_view.has_result_file('model.object.RData.zip', 'gbm'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_Phascolarctus.cinereus.tif', 'gbm'))
+        self.assertTrue(experiment_view.has_result_file('pstats.json', 'gbm'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_ClampingMask.tif', 'gbm'))
+
+        # Cleanup
+        self.delete_experiment(experiment_name)
+
+    # Test 4 algorithms together.
+    # Quad Climate Layers
+    # 1KM
+    def test_gam_circles_ch_bioclim_1km_quad(self):
+        homepage = Homepage(self.driver)
+        login_page = homepage.click_login()
+        homepage = login_page.valid_login(self.username, self.password)
+        experiment_page = homepage.click_experiments()
+        new_sdm_page = experiment_page.click_new_sdm_experiment()
+
+        experiment_name = "4exp_quadlayer_" + generate_timestamp()
+
+        new_sdm_page.enter_experiment_name(experiment_name)
+        new_sdm_page.enter_experiment_description('gam_circles_ch_bioclim_1km_quad')
+        new_sdm_page.select_configuration()
+        new_sdm_page.select_sdm_algorithm('Circles')
+        new_sdm_page.select_sdm_algorithm('Bioclim')
+        new_sdm_page.select_sdm_algorithm('Convex Hull')
+        new_sdm_page.select_sdm_algorithm('Generalized Additive Model')
+        new_sdm_page.select_occurrences()
+        new_sdm_page.select_occurrences_dataset('Koala - Mini occurrence dataset for Redland City')
+        new_sdm_page.select_absences()
+        new_sdm_page.select_absences_dataset('Koala - Mini absence dataset for Redland City')
+        new_sdm_page.select_environment()
+
+        new_sdm_page.select_current_climate_layers('Current climate layers for Redland City, 30" (~1km)')
+        new_sdm_page.select_environmental_datasets('Current climate layers for Redland City, 30" (~1km)',
+                                                   'B01 - Annual Mean Temperature')
+        new_sdm_page.select_environmental_datasets('Current climate layers for Redland City, 30" (~1km)',
+                                                   'B02 - Mean Diurnal Range (Mean of monthly (max temp - min temp))')
+        new_sdm_page.select_environmental_datasets('Current climate layers for Redland City, 30" (~1km)',
+                                                   'B14 - Precipitation of Driest Month')
+        new_sdm_page.select_environmental_datasets('Current climate layers for Redland City, 30" (~1km)',
+                                                   'B09 - Mean Temperature of Driest Quarter')
+
+        new_sdm_page.select_run()
+        experiment_view = new_sdm_page.select_review_start_experiment()
+
+        # Wait until completion
+        experiment_view.wait_for_experiment_to_complete()
+        self.assertTrue(experiment_view.has_completed_successfully())
+
+        self.assertTrue(experiment_view.has_results_header(experiment_name))
+
+        self.assertEqual(58, experiment_view.get_num_output_files())
+
+        self.assertTrue(experiment_view.has_result_file('dismo.eval.object.RData', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('Phascolarctus.cinereus.model.object.RData', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_09_response.png', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_01_response.png', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_02_response.png', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_14_response.png', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('results.html', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('AUC.png', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('bioclim.Rout', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('combined.modelEvaluation.csv', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('maxent_like_VariableImportance.csv', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('biomod2_like_VariableImportance.csv', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('results.html.zip', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_Phascolarctus.cinereus.tif', 'bioclim'))
+        self.assertTrue(experiment_view.has_result_file('pstats.json', 'bioclim'))
+
+        self.assertTrue(experiment_view.has_result_file('dismo.eval.object.RData', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('Phascolarctus.cinereus.model.object.RData', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_09_response.png', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('lon_response.png', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('lat_response.png', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_01_response.png', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_02_response.png', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_14_response.png', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('results.html', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('AUC.png', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('circles.Rout', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('combined.modelEvaluation.csv', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('maxent_like_VariableImportance.csv', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('biomod2_like_VariableImportance.csv', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('results.html.zip', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('pstats.json', 'circles'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_Phascolarctus.cinereus.tif', 'circles'))
+
+        self.assertTrue(experiment_view.has_result_file('dismo.eval.object.RData', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('Phascolarctus.cinereus.model.object.RData', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_09_response.png', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('lon_response.png', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('lat_response.png', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_01_response.png', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_02_response.png', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('bioclim_14_response.png', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('results.html', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('AUC.png', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('convhull.Rout', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('combined.modelEvaluation.csv', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('maxent_like_VariableImportance.csv', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('biomod2_like_VariableImportance.csv', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('results.html.zip', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_Phascolarctus.cinereus.tif', 'convhull'))
+        self.assertTrue(experiment_view.has_result_file('pstats.json', 'convhull'))
+
+        self.assertTrue(experiment_view.has_result_file('mean_response_curves.png', 'gam'))
+        self.assertTrue(experiment_view.has_result_file('pROC.png', 'gam'))
+        self.assertTrue(experiment_view.has_result_file('gam.Rout', 'gam'))
+        self.assertTrue(experiment_view.has_result_file('biomod2.modelEvaluation.csv', 'gam'))
+        self.assertTrue(experiment_view.has_result_file('combined.modelEvaluation.csv', 'gam'))
+        self.assertTrue(experiment_view.has_result_file('model.object.RData.zip', 'gam'))
+        self.assertTrue(experiment_view.has_result_file('pstats.json', 'gam'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_Phascolarctus.cinereus.tif', 'gam'))
+        self.assertTrue(experiment_view.has_result_file('proj_current_ClampingMask.tif', 'gam'))
+
+        # Cleanup
+        self.delete_experiment(experiment_name)
