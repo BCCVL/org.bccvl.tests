e745f17d8c62a44deb598e4c470e99576d959129
Gerhard Weis <g.weis@griffith.edu.au>
2014-08-19 12:12:18 +1000

    add switch_tab convenience method which waits for all ajax calls to finish

--- functional_tests/pages/base_page.py
+++ functional_tests/pages/base_page.py
@@ -1,5 +1,16 @@
+from selenium.webdriver.support.ui import WebDriverWait
+
+
 class BasePage():
 
     def __init__(self, driver):
         self.driver = driver
         self.title = driver.title
+
+    def is_jquery_active(self):
+        return self.driver.execute_script('return jQuery.active') != 0
+
+    def switch_tab(self, link_text):
+        self.driver.find_element_by_link_text(link_text).click()
+        # wait for ajax calls to finish
+        WebDriverWait(self.driver, 10).until(lambda s: not self.is_jquery_active())
